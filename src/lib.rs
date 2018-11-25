09613198b80318406a24f5409b5df627d9f01182
statiolake <statiolake@gmail.com>
2018-11-26 03:30:41 +0900

    implement current_exe

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,20 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+pub use to_absolute::Result;
+
+use to_absolute::to_absolute;
+
+use std::env;
+use std::fs;
+use std::path::PathBuf;
+
+pub fn current_exe() -> Result<PathBuf> {
+    let current_exe = env::current_exe()?;
+    match fs::read_link(&current_exe) {
+        Err(_) => Ok(current_exe),
+        Ok(actual_exe) => to_absolute(
+            current_exe
+                .parent()
+                .expect("FILE must have the parent directory!"),
+            actual_exe,
+        ),
     }
 }
